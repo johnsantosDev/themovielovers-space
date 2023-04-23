5103d4c0279a72de612038cc1c6b0899280f894b
johnsantosDev <jonndiwayesusantos@gmail.com>
2023-04-23 12:09:11 +0200

    creating home ui, defining  api keys and vite settings

--- src/components/TheHeroSection.vue
+++ src/components/TheHeroSection.vue
@@ -2,15 +2,38 @@
 import { RouterLink } from 'vue-router'
 </script>
 <template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="logo.png" alt="Logo" height="50" class="d-inline-block align-text-top">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+  <ul class="navbar-nav ms-auto">
+    <li class="nav-item">
+      <RouterLink class="nav-link" to="/">Home</RouterLink>
+    </li>
+    <li class="nav-item">
+      <RouterLink class="nav-link" to="/all-movies-sample">Movies</RouterLink>
+    </li>
+    <li class="nav-item">
+      <RouterLink class="nav-link" to="/saved-list">TV Shows</RouterLink>
+    </li>
+  </ul>
+</div>
+
+  </div>
+</nav>
+
   <section class="hero bg-primary text-white justify-content-center d-flex">
     <div class="container col-xxl-8 px-4 py-5">
       <div class="row flex-lg-row align-items-center g-5 py-5 h-100">
         <div class="col-lg-6 shake">
-          <h1 class="display-5 fw-bold lh-1 mb-3">Welcome to our movie rating website,</h1>
+          <h1 class="display-5 fw-bold lh-1 mb-3">Looking for the ultimate destination for movie lovers?</h1>
           <p class="lead">
-            the ultimate destination for movie lovers! Discover new releases, explore your favorite
-            genres, and rate your favorite movies. With a vast collection of titles, you're sure to
-            find something that suits your taste.
+            Look no further than our movie rating website! Whether you're searching for the latest releases or your favorite classics, we have it all. With an extensive collection of titles spanning a variety of genres, you're sure to find something that suits your taste.
           </p>
           <div class="d-grid gap-2 d-md-flex justify-content-md-start">
             <RouterLink
--- src/requests/index.js
+++ src/requests/index.js
@@ -1,62 +1,63 @@
 import axios from 'axios'
 import _ from 'lodash'
 
-const api_key = import.meta.env.VITE_API_KEY
+const api_key =
+    import.meta.env.VITE_API_KEY
 
-const movieList = async function (page = 1) {
-  try {
-    const response = await axios.get(
-      `https://www.omdbapi.com/?apikey=${api_key}&type=movie&y=2023&s=movie&page=${page}&plot=full`
-    )
-    const { data } = response
-    const { Search } = data
-    return _.shuffle(Search)
-  } catch (error) {
-    throw new Error(error)
-  }
+const movieList = async function(page = 1) {
+    try {
+        const response = await axios.get(
+            `https://www.omdbapi.com/?apikey=${api_key}&type=movie&y=2023&s=movie&page=${page}&plot=full`
+        )
+        const { data } = response
+        const { Search } = data
+        return _.shuffle(Search)
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
-const searchMovie = async function (page = 1, search = 'a', year = '') {
-  try {
-    const response = await axios.get(
-      `https://www.omdbapi.com/?apikey=${api_key}&type=movie&s=${
+const searchMovie = async function(page = 1, search = 'a', year = '') {
+    try {
+        const response = await axios.get(
+            `https://www.omdbapi.com/?apikey=${api_key}&type=movie&s=${
         search.length == 0 ? 'a' : search
       }&page=${page}&y=${year}&plot=full`
-    )
-    const { data } = response
-    if (data.Response == 'False') {
-      throw new Error(data.Error)
+        )
+        const { data } = response
+        if (data.Response == 'False') {
+            throw new Error(data.Error)
+        }
+        const { Search } = data
+        return Search
+    } catch (error) {
+        throw new Error(error)
     }
-    const { Search } = data
-    return Search
-  } catch (error) {
-    throw new Error(error)
-  }
 }
 
-const singleMovie = async function (imdbID) {
-  if (!imdbID) {
-    throw new Error('Please specify a correct imdbID')
-  }
-  try {
-    const response = await axios.get(
-      `https://www.omdbapi.com/?apikey=${api_key}&type=movie&i=${imdbID}&plot=full`
-    )
-    const { data } = response
-    if (typeof data === 'string') {
-      if (data.indexOf('"Response":"False"') !== -1) {
-        throw new Error('Invalid movie ID')
-      }
+const singleMovie = async function(imdbID) {
+    if (!imdbID) {
+        throw new Error('Please specify a correct imdbID')
     }
-    if (typeof data === 'object') {
-      if (data.Response == 'False') {
-        throw new Error(data.Error)
-      }
+    try {
+        const response = await axios.get(
+            `https://www.omdbapi.com/?apikey=${api_key}&type=movie&i=${imdbID}&plot=full`
+        )
+        const { data } = response
+        if (typeof data === 'string') {
+            if (data.indexOf('"Response":"False"') !== -1) {
+                throw new Error('Invalid movie ID')
+            }
+        }
+        if (typeof data === 'object') {
+            if (data.Response == 'False') {
+                throw new Error(data.Error)
+            }
+        }
+        return data
+    } catch (error) {
+        throw new Error(error)
     }
-    return data
-  } catch (error) {
-    throw new Error(error)
-  }
 }
 
 export { movieList, searchMovie, singleMovie }
