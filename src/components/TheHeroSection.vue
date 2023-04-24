7871caa521d066d007a818ae1e984cad5582ef34
johnsantosDev <jonndiwayesusantos@gmail.com>
2023-04-24 18:48:30 +0200

    Fix build issues for netlify deployment

--- src/components/TheHeroSection.vue
+++ src/components/TheHeroSection.vue
@@ -5,7 +5,7 @@ import { RouterLink } from 'vue-router'
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
-      <img src="logo.png" alt="Logo" height="50" class="d-inline-block align-text-top">
+      <img src="/logo.png" alt="Logo" height="50" class="d-inline-block align-text-top">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
